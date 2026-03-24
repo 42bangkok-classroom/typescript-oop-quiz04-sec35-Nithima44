69a31355988577ae56177699217926f15cd90edd
Nithima44 <nitima7859@gmail.com>
2026-03-25 03:50:15 +0700

    3

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
     return [];
   }
 
-  private readonly filePath = path.join(process.cwd(), 'data', 'users.json');
+  private readonly filePath = path.join(__dirname, '../../data/users.json');
 
   private async readData(): Promise<IUser[]> {
     const data = await fs.readFile(this.filePath, 'utf-8');
