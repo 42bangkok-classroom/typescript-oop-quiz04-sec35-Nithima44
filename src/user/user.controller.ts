4a598812a2a1c710f5122115ab4d2950e7fa9b3a
Nithima44 <nitima7859@gmail.com>
2026-03-25 01:48:00 +0700

    2

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import {Get , Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    gettest() {
+        return this.userService.test();
+    }
+}
