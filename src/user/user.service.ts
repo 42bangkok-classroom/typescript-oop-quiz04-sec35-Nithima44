fee524a49bdff6a24d5c8792e71e4994a291e41f
Nithima44 <nitima7859@gmail.com>
2026-03-25 02:56:42 +0700

    3

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
 import { IUser } from './user.interface';
 import * as path from 'path';
 
@@ -9,9 +9,9 @@ export class UserService {
     return [];
   }
 
-  async findAll(): Promise<IUser[]> {
+  findAll(): IUser[] {
     const filePath = path.join(process.cwd(), 'data', 'users.json');
-    const data = await fs.readFile(filePath, 'utf-8');
+    const data = fs.readFileSync(filePath, 'utf-8');
     const users: IUser[] = JSON.parse(data);
     return users;
   }
